f8e3cbe8dfdc9e1bd43f725914aa1f87ab2027ef
spencerrfree <sf376@byu.edu>
2023-02-23 14:43:39 -0700

    Everything except migrations...

---
+++ Models/TaskContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Mission_08_group_1_1.Models
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Tasks> Tasks { get; set; }
+        public DbSet<Category> Catergories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+
+            mb.Entity<Category>().HasData(
+                new Category { CatergoryId = 1, CatergoryName = "Home" },
+                new Category { CatergoryId = 2, CatergoryName = "School" },
+                new Category { CatergoryId = 3, CatergoryName = "Work" },
+                new Category { CatergoryId = 4, CatergoryName = "Church" }
+                );
+
+            mb.Entity<Tasks>().HasData(
+                new Tasks
+                {
+                    Task = "Do the dishes",
+                    CategoryId = 1,
+                    DueDate = DateTime.Now,
+                    Quadrant = 4,
+                    Completed = false,
+                    TaskId = 1
+                }
+            );
+        }
+    }
+
+}
---
+++ Models/Tasks.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Mission_08_group_1_1.Models 
+{
+	public class Tasks
+	{
+			[Key]
+			[Required]
+			public int TaskId { get; set; }
+
+			[Required]
+			public string Task { get; set; }
+
+			public DateTime DueDate { get; set; }
+
+			[Required]
+			public int Quadrant { get; set; }
+
+			public string Catergory { get; set; }
+
+			public bool Completed { get; set; }
+
+			[Required]
+			public int CategoryId { get; set; }
+			public Category Category { get; set; }
+		}
+	}
